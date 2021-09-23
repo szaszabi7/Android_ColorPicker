1b86ae66f70fcb34590c7ccb9853c84406dbe63d
Szalai <sz.szalai16@gmail.com>
2021-09-23 19:48:34 +0200

    Seekbar működik, szín változik, szöveg olvasható

--- app/src/main/java/hu/petrik/MainActivity.kt
+++ app/src/main/java/hu/petrik/MainActivity.kt
@@ -1,5 +1,6 @@
 package hu.petrik
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Layout
@@ -15,12 +16,66 @@ class MainActivity : AppCompatActivity() {
     lateinit var textView: TextView
     lateinit var layout : RelativeLayout
 
+    var red = 0;
+    var green = 0;
+    var blue = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
+        redSeek.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                red = redSeek.progress
+                textView.setText("(" +red.toString() + "," + green.toString() + "," + blue.toString() + ")")
+                layout.setBackgroundColor(Color.rgb(red, green, blue))
+                if (red + green + blue > 300) textView.setTextColor(Color.BLACK)
+                else textView.setTextColor(Color.WHITE)
+            }
 
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+
+        greenSeek.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                green = greenSeek.progress
+                textView.setText("(" +red.toString() + "," + green.toString() + "," + blue.toString() + ")")
+                layout.setBackgroundColor(Color.rgb(red, green, blue))
+                if (red + green + blue > 300) textView.setTextColor(Color.BLACK)
+                else textView.setTextColor(Color.WHITE)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+
+        blueSeek.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                blue = blueSeek.progress
+                textView.setText("(" +red.toString() + "," + green.toString() + "," + blue.toString() + ")")
+                layout.setBackgroundColor(Color.rgb(red, green, blue))
+                if (red + green + blue > 300) textView.setTextColor(Color.BLACK)
+                else textView.setTextColor(Color.WHITE)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
     }
+
 
 
 
